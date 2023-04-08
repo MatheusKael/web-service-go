fa42e3858ab8d386f88dfcb44667d929c3a1978c
MatheusKael <matheuskael.silva@gmail.com>
2023-04-08 11:30:32 -0400

    initial commit

---
+++ service.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue train", Artist: "John Coltrane"},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+
+}
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+
+}
